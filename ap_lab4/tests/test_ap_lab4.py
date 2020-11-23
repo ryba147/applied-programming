36b2e9d1b7fb843f7d14768014e67d3990e284dd
ryba147 <54861049+ryba147@users.noreply.github.com>
2020-11-23 11:53:48 +0200

    Swagger API Documentation

--- ap_lab4/tests/test_ap_lab4.py
+++
@@ -1,5 +0,0 @@
-from ap_lab4 import __version__
-
-
-def test_version():
-    assert __version__ == '0.1.0'
--- ap_lab4/wsgi.py
+++
@@ -1,6 +0,0 @@
-from myproject import application
-
-# gunicorn wsgi:application
-# gunicorn -w 2 -b 0.0.0.0:8080 myproject:application
-if __name__ == "__main__":
-    application.run()
