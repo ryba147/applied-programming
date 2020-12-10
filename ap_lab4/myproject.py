bb94241decb7a16a10d3fe4c270662ee4371eac5
PaulMoroz <55598887+PaulMoroz@users.noreply.github.com>
2020-12-10 10:33:45 +0200

    Add files via upload

--- ap_lab4/myproject.py
+++ ap_lab4/myproject.py
@@ -1,8 +1,59 @@
 from flask import Flask
-from flask import render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager,Server
+from flask_migrate import Migrate, MigrateCommand
 
 application = Flask(__name__)
-v = 3  # variant by default
+
+db = SQLAlchemy(application)
+
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lab.db'
+V = 3
+
+migrate = Migrate(application, db)
+
+class User(db.Model):
+    #__tablename__ = 'User'
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    firstname = db.Column(db.String(64),nullable=False)
+    lastname = db.Column(db.String(64),nullable=False)
+    password = db.Column(db.String(64),nullable=False)
+    location = db.Column(db.String(64),nullable=False)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.id)
+
+class AnnouncementType(db.Model):
+    __tablename__ = 'AnnouncementType'
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
+    description = db.Column(db.String(64),nullable=False)
+    def __repr__(self):
+        return '<AnnouncementType {}>'.format(self.id)
+
+
+class Announcement(db.Model):
+    #__tablename__ = 'Announcement'
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
+    authorid = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    name = db.Column(db.String(64),nullable=False)
+    description = db.Column(db.Text,nullable=False)
+    pub_date = db.Column(db.String(11),nullable=False)
+    location = db.Column(db.Integer,db.ForeignKey('location.id'),nullable=False)
+    announcement_type = db.Column(db.Integer,db.ForeignKey('AnnouncementType.id') ,nullable=False)
+    def __repr__(self):
+        return '<Announcement {}>'.format(self.id)
+
+
+class Location(db.Model):
+    #__tablename__ = 'Location'
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    def __repr__(self):
+        return '<Location {}>'.format(self.id)
+
+
+
 
 
 @application.route('/')
@@ -15,5 +66,7 @@ def hello_world(url_v):
     return render_template('hello.html', var=url_v)  # or return 'Hello World!'
 
 
+
 if __name__ == '__main__':
-    application.run(host='0.0.0.0')
+    application.run()
+
